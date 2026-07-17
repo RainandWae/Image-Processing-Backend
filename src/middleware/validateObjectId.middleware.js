08d11c416fdff7752d919258dc8abbadb1622298
RainandWae <minhvuquang1021@gmail.com>
2026-07-17 08:14:16 +0700

    Add image ObjectId validation

---
+++ src/middleware/validateObjectId.middleware.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const validateObjectId = (paramName = 'id') => {
+    return (req, res, next) => {
+        const value = req.params[paramName];
+
+        if (!mongoose.Types.ObjectId.isValid(value)) {
+            return res.status(400).json({
+                message: `Invalid ${paramName}`,
+            });
+        }
+
+        next();
+    };
+};
+
+module.exports = validateObjectId;
--- src/routes/image.routes.js
+++ src/routes/image.routes.js
@@ -11,20 +11,21 @@ const {
 
 const validateTransform = require('../middleware/validateTransform.middleware');
 const { transformRateLimiter } = require('../middleware/rateLimit.middleware');
+const validateObjectId = require('../middleware/validateObjectId.middleware');
 
 const router = express.Router();
 
 router.post('/images', protect, upload.single('image'), uploadImage);
 router.get('/images', protect, listImages);
-router.get('/images/:id', protect, getImageById);
-router.delete('/images/:id', protect, deleteImage);
+router.get('/images/:id', protect, validateObjectId('id'), getImageById);
+router.delete('/images/:id', protect, validateObjectId('id'), deleteImage);
 
 router.post(
   '/images/:id/transform',
   protect,
+  validateObjectId('id'),
   transformRateLimiter,
   validateTransform,
-  
   transformImage
 );
 
